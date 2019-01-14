e7643a5017100393de4a6a0cc52988eb06c5c780
Gregg Hosie <gregg.hosie@gmail.com>
2019-01-13 22:22:20 -0500

    initial html skeleton, login trial

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+var config = {
+  apiKey: "AIzaSyB5zgm0cMycq5Go4XObni0kGPvFl8EjRAQ",
+  authDomain: "angler-c5724.firebaseapp.com",
+  databaseURL: "https://angler-c5724.firebaseio.com",
+  projectId: "angler-c5724",
+  storageBucket: "angler-c5724.appspot.com",
+  messagingSenderId: "423460196917"
+};
+firebase.initializeApp(config);
+
+var uiConfig = {
+  signInSuccessUrl: 'https://greggh88.github.io/AnglerDemo/',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign('<your-privacy-policy-url>');
+  }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
